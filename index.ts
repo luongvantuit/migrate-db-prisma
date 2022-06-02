871f179876fcc5cc5beca72ca0dfc1e0b1ee05ae
Lương Văn Tú <luongvantuit@gmail.com>
2022-06-02 12:56:08 +0700

    test mongodb atlas

--- index.ts
+++ index.ts
@@ -1,23 +1,31 @@
 import { PrismaClient } from "@prisma/client";
+import { join } from "path";
+import { config } from "dotenv";
+
+config({
+  path: join(process.cwd(), ".env"),
+});
 
 const prisma = new PrismaClient();
 
 async function main() {
   await prisma.$connect();
-
-  await prisma.personModel.create({
-    data: {
-      name: "John",
-      email: "john@example.com",
-      phone: "123",
-      dob: "John",
-    },
-  });
 }
 
 main()
   .catch(async (e) => {
     await prisma.$disconnect();
+    console.log(e);
+
     console.log("failed to connect");
   })
-  .then(() => {});
+  .then(async () => {
+    await prisma.personModel.create({
+      data: {
+        name: "John",
+        email: "john@example.com",
+        phone: "123",
+        dob: "John",
+      },
+    });
+  });
